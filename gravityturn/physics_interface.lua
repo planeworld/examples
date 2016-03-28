017ba78dcd8c1af5af891036745fce1d769b550d
Sven Klomp <mail@klomp.eu>
2016-03-28 15:42:48 +0200

    Change Lua to use newly intriduced pw package

--- gravityturn/physics_interface.lua
+++ gravityturn/physics_interface.lua
@@ -7,16 +7,16 @@ Object["name"] = "RocketBody"
 ObjectRef = {}
 ObjectRef["name2"] = "Earth"
 
-F=12900000.0*1.062
+F=44272463.35156199
 thrust=true
 
 function physics_interface()
     
-    frequency = get_frequency()
+    frequency = pw.system.get_frequency()
     Frametime = 1.0/frequency
     
-    Object["p_x"], Object["p_y"] = get_position(Object["name"])
-    Object["v_x"], Object["v_y"] = get_velocity(Object["name"])
+    Object["p_x"], Object["p_y"] = pw.universe.get_position(Object["name"])
+    Object["v_x"], Object["v_y"] = pw.universe.get_velocity(Object["name"])
    
     v_abs=math.sqrt(Object["v_x"]^2+Object["v_y"]^2)
     r_abs=math.sqrt(Object["p_x"]^2+Object["p_y"]^2)
@@ -32,7 +32,7 @@ function physics_interface()
     
     if thrust then
       if v_r>0 then
-        apply_force(Object["name"], F_x, F_y , 0.0, 0.0)
+        pw.universe.apply_force(Object["name"], F_x, F_y , 0.0, 0.0)
       else
         thrust=false
         io.write("Engine stopped at ", r_abs,"m \n")
