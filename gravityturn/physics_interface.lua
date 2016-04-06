521514ec5464eb6a3ba2b10024ed0a13f6fc1258
Sven Klomp <mail@klomp.eu>
2016-04-06 21:04:15 +0200

    Move Lua functions into separate module

--- gravityturn/physics_interface.lua
+++ gravityturn/physics_interface.lua
@@ -1,48 +1,5 @@
-
-pwt= {};
-pwt.universe={};
-
-pwt.universe.get_object = function(name)
-  local object = {};
-  object.name=name;
-  object.mass = pw.universe.get_mass(name);
-  object.inertia = pw.universe.get_inertia(name);
-  
-  object.get_position = function()
-    return pw.universe.get_position(name);
-  end
-  
-  object.get_velocity = function()
-    return pw.universe.get_velocity(name);
-  end
-  
-  object.get_angle = function()
-    return pw.universe.get_angle(name);
-  end
-
-  object.get_angle_vel = function()
-    return pw.universe.get_angle_vel(name);
-  end
-  
-  return object;
-end
-
-
-pwt.universe.get_component = function(name)
-  local object = {};
-  object.name=name;
-  
-  object.set_force = function(force)
-    if (force<0.0) then
-      pw.sim.deactivate_thruster(name);
-    else
-      pw.sim.activate_thruster(name, force);
-    end
-  end
-  
-  return object;
-end
-
+io.open("testfile.txt", "w")
+pwt= require "gravityturn/pwt"
 
 
 objects = {}
