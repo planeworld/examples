24eb5ee866782277f48312ff9f78f5d3769baedf
Sven Klomp <mail@klomp.eu>
2017-12-09 09:10:02 +0100

    Change example to use absolute angle velocity

--- sputnik_example.lua
+++ sputnik_example.lua
@@ -3,6 +3,8 @@
 -- Import input script
 require('pw_input_default')
 
+astrodynamics = require('lua_modules.astrodynamics')
+
 -- Setup engine
 pw.system.set_frequency_input(100)
 pw.system.set_frequency_lua(30)
@@ -10,6 +12,26 @@ pw.system.set_frequency_physics(200)
 pw.system.set_frequency_visuals(60)
 pw.system.set_data_path_visuals("./")
 pw.system.create_universe(23479, 10000)
+
+-- Create earth
+earth = {}
+earth.mass= 5.97219e24
+earth.radius = 6378.16e3
+-- 23h56m4.1s
+earth.angle_vel = 2 * math.pi / ((23*60 + 56)*60 + 4.1)
+idObjEarth=pw.system.create_obj()
+pw.physics.obj_set_name(idObjEarth, "Earth")
+pw.physics.obj_set_angle_vel(idObjEarth, earth.angle_vel)
+idShpEarth=pw.system.create_shp("shp_planet")
+pw.physics.shp_set_radius(idShpEarth, earth.radius)
+pw.physics.shp_set_mass(idShpEarth, earth.mass)
+-- height_max="8000.0"
+-- sea_level="-0.2"
+-- ground_resolution="0.001"
+-- inertia="8.0e37"
+-- gravity="true"
+pw.system.obj_add_shp(idObjEarth, idShpEarth)       
+pw.physics.obj_disable_dynamics(idObjEarth)
 
 -- Create Sputnik
 idObjSputnik = pw.system.create_obj()
@@ -41,10 +63,12 @@ pw.system.obj_add_shp(idObjSputnik, idShpSputnikAntenna1)
 pw.system.obj_add_shp(idObjSputnik, idShpSputnikAntenna2)
 pw.system.obj_add_shp(idObjSputnik, idShpSputnikAntenna3)
 
---pw.physics.obj_set_name(idObjSputnik, "Sputnik")
-pw.physics.obj_set_position(idObjSputnik, 0.0, 6954607.812238137)
+pw.physics.obj_set_name(idObjSputnik, "Sputnik")
+local orbit_height = 6954607.812238137
+local velocity = math.sqrt(astrodynamics.G*earth.mass/orbit_height)
+pw.physics.obj_set_position(idObjSputnik, 0.0, orbit_height)
 pw.physics.obj_set_angle(idObjSputnik, math.rad(90))
-pw.physics.obj_set_velocity(idObjSputnik, -7064.774995603303, 0.0)
+pw.physics.obj_set_velocity(idObjSputnik, -velocity, 0.0)
 pw.physics.obj_set_angle_vel(idObjSputnik, 0.0010158408908653759)
 -- kinematics_reference="Earth"
 -- dynamics="true"
@@ -52,20 +76,7 @@ pw.physics.obj_set_angle_vel(idObjSputnik, 0.0010158408908653759)
 -- inertia="100.0"
 
 
-idObjEarth=pw.system.create_obj()
---pw.physics.obj_set_name(idObjEarth, "Earth")
-pw.physics.obj_set_angle_vel(idObjEarth, 0.0000727220521664304)
-idShpEarth=pw.system.create_shp("shp_planet")
-pw.physics.shp_set_radius(idShpEarth, 6378.16e3)
-pw.physics.shp_set_mass(idShpEarth, 5.97219e24)
--- height_max="8000.0"
--- sea_level="-0.2"
--- ground_resolution="0.001"
--- inertia="8.0e37"
--- dynamics="true"
--- gravity="true"
-pw.system.obj_add_shp(idObjEarth, idShpEarth)       
-pw.physics.obj_disable_dynamics(idObjEarth)
+
 
 
 
